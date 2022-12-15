5f148058189af100981cc608b65dec0b5c73fa2c
piazin <ls4803326@gmail.com>
2022-12-14 22:24:31 -0300

    remove test job

--- dist/jobs/testJob.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.job = void 0;
-const cron_1 = require("cron");
-exports.job = new cron_1.CronJob('* */10 * * * ', () => {
-    console.info('job send 5s');
-});
-//# sourceMappingURL=testJob.js.map
