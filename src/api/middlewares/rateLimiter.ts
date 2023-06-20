d64e034c6270a538cbe849577738d48ce7193496
Lucas Souza <lucas@pop-os.localdomain>
2023-06-20 12:04:28 -0300

    test deploy in render

--- src/api/middlewares/rateLimiter.ts
+++ src/api/middlewares/rateLimiter.ts
@@ -1,12 +1,25 @@
 import Redis from 'ioredis';
+import config from '../../config';
 import { RequestHandler } from 'express';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 
-const redisClient = new Redis({ enableOfflineQueue: false });
+const {
+  redis: { username, password, host, port },
+} = config;
+
+const redisClient = new Redis({
+  host,
+  port,
+  username,
+  password,
+  tls: {},
+});
 
 redisClient.on('error', (err) => {
   console.error('🚀 ~ file: rateLimiter.ts:10 ~ redisClient.on ~ err:', err);
 });
+
+redisClient.on('connection', (stream) => console.info('Redis connected'));
 
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,12 @@ export default {
   db: {
     url: process.env.NODE_ENV === 'production' ? process.env.MONGO_URL : process.env.MONGO_DOCKER,
   },
+  redis: {
+    username: process.env.NODE_ENV === 'production' ? null : process.env.REDIS_USER_NAME,
+    password: process.env.NODE_ENV === 'production' ? null : process.env.REDIS_PASSWORD,
+    host: process.env.NODE_ENV === 'production' ? process.env.REDIS_HOST : 'ohio-redis.render.com',
+    port: 6379,
+  },
   email: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
