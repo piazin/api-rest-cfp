c4b052ae8d5f229ef25a7dd1407e851e99f3f7f9
piazin <ls4803326@gmail.com>
2023-06-21 19:07:17 -0300

    :recycle: refactor: add default main route

--- src/routes/use.routes.ts
+++ src/routes/use.routes.ts
@@ -3,5 +3,6 @@ import { userRouter, categoryRouter, transactionRouter, authRouter } from '.';
 
 export const useRoutes = async (app: Express) => {
   const baseUrl = '/api/v1';
+  app.get('/', (req, res) => res.status(200).json());
   app.use(baseUrl, userRouter, authRouter, categoryRouter, transactionRouter);
 };
